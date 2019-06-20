4c40152c32d814eb416988e6c6383770ab4ff843
pike <pikewb@gmail.com>
2019-06-20 14:56:11 +0900

    updated the map to map from the state

--- src/MapView.js
+++ src/MapView.js
@@ -12,28 +12,27 @@ const setCurrentColor = (state, color) => ({
     currentColor: color
 })
 
+const Hex = Honeycomb.extendHex({ size: 5 })
+const Grid = Honeycomb.defineGrid(Hex)
+const grid = Grid.hexagon({radius: 15, center: [15,15]})
+const corners = Hex().corners()
+const colors = ["lightBlue", "darkBlue", "PapayaWhip", "brown", "grey", "white", "pink", "darkgreen", "lightgreen"]
+const colorSvg = (color, points) => (<polygon points={points} style={{stroke: "white", fill: color, transform: "rotateX(40deg)"}} onClick={[setCurrentColor, color]}/>)
 
 export default (state) => {
-    const Hex = Honeycomb.extendHex({ size: 5 })
-    const Grid = Honeycomb.defineGrid(Hex)
-    const corners = Hex().corners()
-    const colors = ["lightBlue", "darkBlue", "PapayaWhip", "brown", "grey", "white", "pink", "darkgreen", "lightgreen"]
-    const colorSvg = (color, points) => (<polygon points={points} style={{stroke: "white", fill: color, transform: "rotateX(40deg)"}} onClick={[setCurrentColor, color]}/>)
     return (<div style={{backgroundColor: "darkBlue", color: "white"}}> Some Map:
         <svg viewBox="-100 0 300 10">
             {Grid.rectangle({height: 1, width: colors.length}).map(hex => (
                 colorSvg(colors[hex.x], corners.map(({x, y}) => `${x + hex.toPoint().x}, ${y + hex.toPoint().y}`) )
             ))}
         </svg>
-        <svg viewBox="10 10 300 300" style={{overflow: "visible"}}>
-            {Grid.hexagon({radius: 15, center: [15,15]}).map(hex => {
-                const curHex = state.hexMap[hex.x][hex.y]
-                const dX = hex.toPoint().x + 15
-                const dY = hex.toPoint().y + 15
-                return <polygon points={corners.map(({x, y}) => `${x + dX}, ${y + dY}`)}
-                                style={{stroke: "black", fill: curHex.color, transform: "rotateX(40deg)"}}
+        <svg viewBox="0 0 300 300" style={{overflow: "visible"}}>
+            {state.hexMap.map((row, y) => row.map((cell, x) => {
+                const hex = grid.get([x, y])
+                return hex && <polygon points={corners.map(({x, y}) => `${x + hex.toPoint().x}, ${y + hex.toPoint().y}`)}
+                                style={{stroke: "black", fill: cell.color, transform: "rotateX(40deg)"}}
                                 onClick={[clickedGrid, [hex.x, hex.y]]}/>
-
+            }))}
         })}
         </svg>
     </div>)
